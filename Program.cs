806d27c388a8bc976f9764fca4ce4f44118c3f9c
Atakan Adalı <atakan_adali@hotmail.com.tr>
2023-03-18 09:58:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasitCarpimTablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("-------------");
+                for (int j = 1; j <= 10; j++)
+                {
+                    z = i * j;
+                    Console.WriteLine(i + " X " + j + " = " + z);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
